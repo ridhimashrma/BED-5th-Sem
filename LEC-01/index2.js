b56f454d8f7e05d452d1b074d2ddf40cd59453c2
ridhimashrma <ridusharma28@gmail.com>
2025-07-03 15:06:45 +0530

    Initial commit

---
+++ LEC-01/index2.js
@@ -0,0 +1,24 @@
+const fs=require("fs");
+console.log(fs);
+console.log("Start");
+setImmediate(()=>{
+    console.log("set Immediate")
+})
+setTimeout(()=>{
+    console.log("Settimeout")
+},0)
+fs.readFile("demo.txt",(data)=>{
+    console.log("file read");
+    setTimeout(() => {
+        console.log("timer 2")
+    },0);
+    setImmediate(() => {
+        console.log("immediate 2")
+    });
+})
+
+console.log("End");
+
+
+//set timeout-> callback and timer 
+//set timeout is running before set immediate why?
