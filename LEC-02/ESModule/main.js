c323f58331ecd35f573832a6d94c4859b0cb5ab1
ridhimashrma <ridusharma28@gmail.com>
2025-07-04 15:22:49 +0530

    ...

---
+++ LEC-02/ESModule/main.js
@@ -0,0 +1,4 @@
+import mul from "./index.js"   //default export
+import {add,sub,div} from "./index.js"  //named export
+console.log(mul);
+console.log(add,sub,div);
--- LEC-02/ESModule/main.mjs
+++
@@ -1,4 +0,0 @@
-import mul from "./index.mjs"   //default export
-import {add,sub,div} from "./index.mjs"  //named export
-console.log(mul);
-console.log(add,sub,div);
