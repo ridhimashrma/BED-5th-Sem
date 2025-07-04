ad3ed3c211c7870ce9ecb0d949771e1d77be3b00
ridhimashrma <ridusharma28@gmail.com>
2025-07-04 14:43:09 +0530

    k

---
+++ LEC-02/file1.js
@@ -0,0 +1,12 @@
+let file2=require("./file2.js")
+console.log(file2)
+
+function add(a,b){
+    return (a+b);
+}
+function sub(a,b){
+    return (a-b);
+}
+
+module.exports.add=add;
+module.exports.sub=sub;
---
+++ LEC-02/main2.js
@@ -0,0 +1,2 @@
+let file= require("./file2.js");
+console.log(file.mul(3,2));
