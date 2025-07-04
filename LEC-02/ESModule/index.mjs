37a935f3b1984e6be24a3dcdc8cf30272c764661
ridhimashrma <ridusharma28@gmail.com>
2025-07-04 15:14:08 +0530

    ..

---
+++ LEC-02/ESModule/index.mjs
@@ -0,0 +1,15 @@
+export function add(a,b){   //named export
+    return (a+b); 
+}
+function mul(a,b){
+    return (a*b);
+}
+export function sub(a,b){    //named export
+    return (a-b);
+}
+export function div(a,b){     //named export
+    return (a/b);
+}
+
+export default mul;
+//export default add;
