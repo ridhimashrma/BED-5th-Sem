e9a1711f70842d1fa5d319ade525670e9194a85c
ridhimashrma <ridusharma28@gmail.com>
2025-08-12 10:56:10 +0530

    :

--- LEC-17/server.js
+++ LEC-17/server.js
@@ -1,11 +1,10 @@
-const exp = require("constants")
 const express=require("express")
 const mongoose =require("mongoose")
 const app=express()
 const Blog=require("./model/blog")
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-//create
+//create -> add
 app.post('/blogs',async(req,res)=>{
     let title=req.body.title;
     let body=req.body.body;
@@ -23,6 +22,28 @@ app.post('/blogs',async(req,res)=>{
     })
 })
 
+//read
+//read all data
+//read selective data
+
+app.get("/blogs",async(req,res)=>{
+    let allBlogs=await Blog.find()
+    res.json({
+        success:true,
+        message:"data added successfully",
+        data:allBlogs
+    })
+})
+
+app.get("/blogs/:id",async(req,res)=>{
+    let id=req.params.id;
+    let blog=await Blog.findById(id);
+    res.json({
+        success:true,
+        message:"blog fetched successfully",
+        data:blog
+    })
+})
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/g27dB')
